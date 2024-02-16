d16ea67af2351ca262d496115f2186763cca0cf5
DevAndreLuis <andredev.github@gmail.com>
2024-02-16 16:48:33 -0300

    Adicionado links no README.md com informações do projeto , adicionado optimações e botões de preview com as imagens dos pokémons

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -4,10 +4,14 @@ const pokemonImage = document.querySelector(".pokemon__image");
 
 const form = document.querySelector(".form");
 const input = document.querySelector(".input__search");
+const butonPrev = document.querySelector(".btn-prev");
+const butonNext = document.querySelector(".btn-next");
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
 
   const data = await APIResponse.json();
@@ -15,19 +19,42 @@ const fetchPokemon = async (pokemon) => {
 };
 
 const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = "Loading...";
+  pokemonNumber.innerHTML = "";
   const data = await fetchPokemon(pokemon);
 
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImage.src =
-    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
-      "front_default"
-    ];
-    input.value ='';
+  if (data) {
+    pokemonImage.style.display = "block";
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+    input.value = "";
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = "none";
+    pokemonName.innerHTML = "not found :c";
+    pokemonNumber.innerHTML = "";
+  }
 };
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  renderPokemon(input.value);
-
+  renderPokemon(input.value.toLowerCase());
 });
+
+butonPrev.addEventListener("click", (event) => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+butonNext.addEventListener("click", (event) => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
---
+++ REDME.md
@@ -0,0 +1,49 @@
+# Pokédex - HTML, CSS, JavaScript
+
+Bem-vindo à Pokédex, uma aplicação web simples, utilizando HTML, CSS e JavaScript. Esta Pokédex permite explorar informações sobre diferentes Pokémon, com a capacidade de avançar, retroceder e pesquisar por nome ou número na caixa de diálogo.
+
+## Prévia
+
+![Pokédex Preview](./Assets/Images/ScreenshotProjeto/Projeto%20imagem.jpg)
+
+## Funcionalidades
+
+- **Consulta Inicial:**
+
+  - Ao abrir a aplicação, você verá as informações do Pokémon de número 1.
+
+- **Navegação:**
+
+  - Utilize os botões "Prev" e "Next" para explorar os Pokémon anteriores e seguintes.
+
+- **Pesquisa:**
+
+  - Digite o nome ou número do Pokémon desejado na caixa de pesquisa e pressione Enter para exibir as informações.
+
+- **Exibição Responsiva:**
+  - O layout se adapta para uma experiência amigável em diferentes dispositivos.
+
+## Personalização
+
+Explore a personalização da aplicação editando os arquivos CSS (./Assets/Css/style.css) e JavaScript (./Assets/Js/script.js). Sinta-se à vontade para adicionar novos recursos, estilos ou ajustar o comportamento conforme sua criatividade.
+
+## Como Usar
+
+1. Abra o arquivo index.html no seu navegador favorito.
+2. Mergulhe na nostalgia e divirta-se explorando a Pokédex!
+
+## Como Contribuir
+
+Contribuições são bem-vindas! Se você tiver melhorias, novos recursos ou encontrar bugs, sinta-se à vontade para enviar pull requests ou abrir issues.
+
+## Autor
+
+- **AndreDev**
+  - GitHub: [DevAndreLuis](https://github.com/DevAndreLuis)
+  - LinkedIn: [AndreDev](https://www.linkedin.com/in/4ndredev/)
+  - Site do projeto : [Site-Online](https://devandreluis.github.io/pokedex/)
+
+## Projeto
+
+- youtube do criador [Projeto-Youtube](https://www.youtube.com/watch?v=SjtdH3dWLa8)
+Agradecimentos ao CANAL Manual do Dev
